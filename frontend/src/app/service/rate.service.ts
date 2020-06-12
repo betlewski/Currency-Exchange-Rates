fbce67860000025a59a5df303126c9cd5960f2fd
betlewski <szymonbetlewski@wp.pl>
2020-06-12 21:32:51 +0200

    Added rate service class

--- frontend/src/app/service/rate.service.ts
+++ frontend/src/app/service/rate.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {Rate} from "../model/rate";
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +17,20 @@ export class RateService {
 
   constructor(private httpClient: HttpClient) { }
 
+  public findAllByShortName(shortName: string): Observable<Rate[]> {
+    return this.httpClient.get<Rate[]>(this.FIND_ALL_BY_SHORTNAME_URL + "/" + shortName);
+  }
+
+  public findAllLast(): Observable<Rate[]> {
+    return this.httpClient.get<Rate[]>(this.FIND_ALL_LAST_URL);
+  }
+
+  public findLastByShortName(shortName: string): Observable<Rate> {
+    return this.httpClient.get<Rate>(this.FIND_LAST_BY_SHORTNAME_URL);
+  }
+
+  public findLastByShortNames(shortName1: string, shortName2: string): Observable<number> {
+    return this.httpClient.get<number>(this.FIND_LAST_BY_SHORTNAMES_URL
+      + "/" + shortName1 + "/" + shortName2);
+  }
 }
